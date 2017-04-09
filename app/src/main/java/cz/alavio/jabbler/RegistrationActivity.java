6bc7f6d3da8553ce20af081314f9df434194807a
Jan Skála <skalaja7@fel.cvut.cz>
2017-04-09 15:59:36 +0200

    Register activity, butterknife.

--- app/src/main/java/cz/alavio/jabbler/RegistrationActivity.java
+++ app/src/main/java/cz/alavio/jabbler/RegistrationActivity.java
@@ -3,16 +3,29 @@ package cz.alavio.jabbler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    //UI components
+    @BindView(R.id.username) EditText username;
+    @BindView(R.id.email) EditText email;
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.confirm_password) EditText confirm_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
     }
 
-    public void onRegisterClick(View v){
+    @OnClick(R.id.register_button)
+    void onRegisterClick(){
 
     }
 }
