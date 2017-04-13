0b2627d978615108718cb627336425eb483100db
Jan Skála <skalaja7@fel.cvut.cz>
2017-04-13 11:48:12 +0200

    Try to register in async task.

--- app/src/main/java/eu/alavio/jabbler/Models/Helper.java
+++ app/src/main/java/eu/alavio/jabbler/Models/Helper.java
@@ -4,18 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by Jan Skála on 12.04.2017.
+ * Helper class for common routines, like validating email or username
  */
 
 public final class Helper {
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
+    private static final Pattern VALID_USERNAME =
+            Pattern.compile("^[a-zA-Z0-9._-]{3,}$", Pattern.CASE_INSENSITIVE);
+
     /**
      * Validates email using regex
      * */
     public static boolean validateEmail(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+        return matcher.find();
+    }
+
+    public static boolean validateUsername(String username){
+        Matcher matcher = VALID_USERNAME.matcher(username);
         return matcher.find();
     }
 }
