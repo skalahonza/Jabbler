35d819c7a2b5e88e272991e46cbc7d465e436c50
Jan Skála <skalaja7@fel.cvut.cz>
2017-04-19 10:04:37 +0200

    Add simple tab view.

--- app/src/main/java/eu/alavio/jabbler/ContactsFragment.java
+++ app/src/main/java/eu/alavio/jabbler/ContactsFragment.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -13,6 +17,8 @@ import android.view.ViewGroup;
  */
 public class ContactsFragment extends Fragment {
 
+    @BindView(R.id.tabHost)
+    TabHost tabHost;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -23,7 +29,21 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        ButterKnife.bind(this, view);
 
+        tabHost.setup();
+
+        TabHost.TabSpec spec1 = tabHost.newTabSpec("Tab 1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Tab 1");
+
+        TabHost.TabSpec spec2 = tabHost.newTabSpec("Tab 2");
+        spec2.setIndicator("Tab 2");
+        spec2.setContent(R.id.tab2);
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        return view;
+    }
 }
