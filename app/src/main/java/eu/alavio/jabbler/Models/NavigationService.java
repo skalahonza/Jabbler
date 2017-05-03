c987eda71388d2abe372764203d098596a9be136
Jan Skála <skalaja7@fel.cvut.cz>
2017-05-04 01:16:13 +0200

    Is fragment displayed.

--- app/src/main/java/eu/alavio/jabbler/Models/NavigationService.java
+++ app/src/main/java/eu/alavio/jabbler/Models/NavigationService.java
@@ -4,6 +4,8 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.util.Log;
 
+import eu.alavio.jabbler.R;
+
 /**
  * Wrapper for android activity/fragment navigation
  */
@@ -28,10 +30,23 @@ public final class NavigationService {
         this.mainNavigationView = mainNavigationView;
     }
 
+    /** Navigates to given fragment, and present it on default navigation view.
+     * @param fragment Fragment to be dispalyed
+     * @param saveInBackstack Save current fragment into backstack before navigating
+     * @param fragmentManager Provide current fragment manager, calling getFragmentManager()
+     * @return True if the navigation was successful
+     */
     public boolean Navigate(Fragment fragment, boolean saveInBackstack, FragmentManager fragmentManager) {
         return Navigate(fragment, saveInBackstack, fragmentManager, mainNavigationView);
     }
 
+    /** Navigates to given fragment, and present it on given navigation view.
+     * @param fragment Fragment to be dispalyed
+     * @param saveInBackstack Save current fragment into backstack before navigating
+     * @param fragmentManager Provide current fragment manager, calling getFragmentManager()
+     * @param navView Navigation view that will be used as a fragment presenter
+     * @return True if the navigation was successful
+     */
     public boolean Navigate(Fragment fragment, boolean saveInBackstack, FragmentManager fragmentManager, int navView) {
         try {
             // Insert the fragment by replacing any existing fragment
@@ -50,5 +65,25 @@ public final class NavigationService {
             Log.e("Navigation service", "Error while navigating", e);
             return false;
         }
+    }
+
+    /** Checks if the fragment is displayed on a given navigation view
+     * @param fragment Fragment to be checked
+     * @param fragmentManager Provide current fragment manager, calling getFragmentManager()
+     * @param frameId Navigation view id that will be observed
+     * @return True fi the fragment is displayed
+     */
+    public boolean isFragmentDisplayed(Fragment fragment, FragmentManager fragmentManager, int frameId) {
+        Fragment f = fragmentManager.findFragmentById(R.id.content_frame);
+        return f.getClass() == fragment.getClass();
+    }
+
+    /** Checks if the fragment is displayed on a default navigation view
+     * @param fragment Fragment to be checked
+     * @param fragmentManager Provide current fragment manager, calling getFragmentManager()
+     * @return True fi the fragment is displayed
+     */
+    public boolean isFragmentDisplayed(Fragment fragment, FragmentManager fragmentManager) {
+        return isFragmentDisplayed(fragment, fragmentManager, mainNavigationView);
     }
 }
