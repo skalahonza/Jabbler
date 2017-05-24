2a3f0d7572bcda82a638f20ddc1b4ec2444cced7
Jan Skála <skalaja7@fel.cvut.cz>
2017-05-24 13:49:13 +0200

    Can navigate to history via more button.

--- app/src/main/java/eu/alavio/jabbler/Fragments/HomeFragment.java
+++ app/src/main/java/eu/alavio/jabbler/Fragments/HomeFragment.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import eu.alavio.jabbler.Models.Helpers.NavigationService;
 import eu.alavio.jabbler.R;
 
 
@@ -23,6 +26,13 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.more_history)
+    void moreHistory() {
+        NavigationService.getInstance().Navigate(NavigationService.MainPages.HISTORY, false, getFragmentManager());
     }
 }
