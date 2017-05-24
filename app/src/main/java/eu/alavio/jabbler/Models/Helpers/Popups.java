38f491bc95ca085e5f5e044f09fcd4c071b251e5
Jan Skála <skalaja7@fel.cvut.cz>
2017-05-24 20:32:26 +0200

    Contact requestt dialog.

--- app/src/main/java/eu/alavio/jabbler/Models/Helpers/Popups.java
+++ app/src/main/java/eu/alavio/jabbler/Models/Helpers/Popups.java
@@ -5,9 +5,16 @@ import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+
+import java.util.function.Consumer;
+
 import eu.alavio.jabbler.Models.API.ApiHandler;
+import eu.alavio.jabbler.Models.API.Friend;
 import eu.alavio.jabbler.R;
 
 /**
@@ -46,6 +53,40 @@ public final class Popups {
         //Cancel clicked
         cancel.setOnClickListener(v -> {
             dialog.dismiss();
+        });
+        dialog.show();
+    }
+
+    public static void contactRequestReceived(Context context, String jid, Consumer<String> deny, Consumer<Friend> confirm) {
+        final Dialog dialog = new Dialog(context);
+
+        dialog.setContentView(R.layout.add_contat_dialog);
+        dialog.setTitle("Custom Alert Dialog");
+
+        TextView jidTb = (TextView) dialog.findViewById(R.id.jid);
+        EditText nickname = (EditText) dialog.findViewById(R.id.name);
+        FloatingActionButton accept = (FloatingActionButton) dialog.findViewById(R.id.accept);
+        FloatingActionButton cancel = (FloatingActionButton) dialog.findViewById(R.id.cancel);
+
+        //Accept clicked
+        accept.setOnClickListener(v -> {
+            try {
+                Friend contact = ApiHandler.getContact(jid);
+                if (contact != null) {
+                    contact.setName(String.valueOf(nickname.getText()));
+                }
+                confirm.accept(contact);
+
+            } catch (SmackException.NotConnectedException | XMPPException.XMPPErrorException | SmackException.NotLoggedInException | SmackException.NoResponseException e) {
+                confirm.accept(null);
+            }
+            dialog.dismiss();
+        });
+
+        //Cancel clicked
+        cancel.setOnClickListener(v -> {
+            dialog.dismiss();
+            deny.accept(jid);
         });
         dialog.show();
     }
